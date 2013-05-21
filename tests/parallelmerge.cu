6128d30c97ea2fb25933bd4ae51565d6926c1276
Sean Baxter <lightborn@gmail.com>
2013-05-21 09:32:14 -0700

    Added parallelmerge (naive) benchmark

--- tests/parallelmerge.cu
+++ tests/parallelmerge.cu
@@ -32,37 +32,95 @@ __global__ void ParallelMergeB(InputIt1 a_global, int aCount, InputIt2 b_global,
 	}
 }
 
+template<typename InputIt1, typename InputIt2, typename OutputIt, typename Comp>
+void ParallelMerge(InputIt1 a_global, int aCount, InputIt2 b_global, int bCount,
+	OutputIt dest_global, Comp comp, CudaContext& context) {
+
+	// NOTE: With NT = 512, limit of 33553920 in either array for Fermi arch.
+	const int NT = 512;
+	int aBlocks = MGPU_DIV_UP(aCount, NT);
+	int bBlocks = MGPU_DIV_UP(bCount, NT);
+
+	ParallelMergeA<NT><<<aBlocks, NT>>>(a_global, aCount, b_global, bCount,
+		dest_global, comp);
+
+	ParallelMergeB<NT><<<bBlocks, NT>>>(a_global, aCount, b_global, bCount, 
+		dest_global, comp);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Benchmark
+
+template<typename T>
+void BenchmarkMergeKeysNaive(int count, int numIt, CudaContext& context) {
+	int aCount = count / 2;
+	int bCount = count - aCount;
+
+	MGPU_MEM(T) a = context.SortRandom<T>(aCount, 0, (T)count);
+	MGPU_MEM(T) b = context.SortRandom<T>(bCount, 0, (T)count);
+	MGPU_MEM(T) c = context.Malloc<T>(count);
+	std::vector<T> aHost, bHost;
+	a->ToHost(aHost);
+	b->ToHost(bHost);
+	std::vector<T> cHost(count);
+		
+	// Benchmark MGPU
+	context.Start();
+	for(int it = 0; it < numIt; ++it)
+		ParallelMerge(a->get(), aCount, b->get(), bCount, c->get(), 
+			mgpu::less<T>(), context);
+	double naiveElapsed = context.Split();
+	
+	// Verify
+	std::merge(aHost.begin(), aHost.end(), bHost.begin(), bHost.end(), 
+		cHost.begin());
+
+	// Compare naive to STL.
+	std::vector<T> cHost2;
+	c->ToHost(cHost2);
+	for(int i = 0; i < count; ++i)
+		if(cHost[i] != cHost2[i]) {
+			printf("MERGE ERROR AT COUNT = %d ITEM = %d\n", count, i);
+			exit(0);
+		}
+		
+	double bytes = 2 * sizeof(T) * count;
+	double naiveThroughput = count * numIt / naiveElapsed;
+	double naiveBandwidth = bytes * numIt / naiveElapsed;
+
+	printf("%s: %9.3lf M/s  %7.3lf GB/s  \n",
+		FormatInteger(count).c_str(), naiveThroughput / 1.0e6,
+		naiveBandwidth / 1.0e9);
+}
+
+const int Tests[][2] = { 
+	{ 10000, 1000 },
+	{ 50000, 1000 },
+	{ 100000, 1000 },
+	{ 200000, 500 },
+	{ 500000, 200 },
+	{ 1000000, 200 },
+	{ 2000000, 200 },
+	{ 5000000, 200 },
+	{ 10000000, 100 },
+	{ 20000000, 100 }
+};
+const int NumTests = sizeof(Tests) / sizeof(*Tests);
 
 int main(int argc, char** argv) {
 	ContextPtr context = CreateCudaDevice(argc, argv, true);
 
-	int ACount = 100;
-	int BCount = 100;
-	int Count = ACount + BCount;
+	typedef int T1;
+	typedef int64 T2;
 
-	MGPU_MEM(int) aData = context->SortRandom<int>(ACount, 0, Count - 1);
-	MGPU_MEM(int) bData = context->SortRandom<int>(BCount, 0, Count - 1);
-	MGPU_MEM(int) destData = context->Malloc<int>(Count);
+	printf("Benchmarking merge-keys on type %s.\n", TypeIdName<T1>());
+	for(int test = 0; test < NumTests; ++test)
+		BenchmarkMergeKeysNaive<T1>(Tests[test][0], Tests[test][1], *context);
 
-	const int NT = 256;
-	int aBlocks = MGPU_DIV_UP(ACount, NT);
-	int bBlocks = MGPU_DIV_UP(BCount, NT);
-
-	ParallelMergeA<NT><<<aBlocks, NT>>>(aData->get(), ACount, bData->get(),
-		BCount, destData->get(), mgpu::less<int>());
-
-	ParallelMergeB<NT><<<bBlocks, NT>>>(aData->get(), ACount, bData->get(),
-		BCount, destData->get(), mgpu::less<int>());
-
-	printf("A data:\n");
-	PrintArray(*aData, "%4d", 10);
-
-	printf("\nB data:\n");
-	PrintArray(*bData, "%4d", 10);
-
-	printf("\nMerged data:\n");
-	PrintArray(*destData, "%4d", 10);
-
+	printf("\nBenchmarking merge-keys on type %s.\n", TypeIdName<T2>());
+	for(int test = 0; test < NumTests; ++test)
+		BenchmarkMergeKeysNaive<T2>(Tests[test][0], Tests[test][1], *context);
+	
 	return 0;
 }
 

7d28d154f3f1ed088d4068e8a0c238932ba659ad
Sean Baxter <lightborn@gmail.com>
2018-01-04 20:30:58 -0500

    More consistent comp passing convention

--- src/moderngpu/kernel_segsort.hxx
+++ src/moderngpu/kernel_segsort.hxx
@@ -72,7 +72,7 @@ struct segsort_t {
   template<bool sort_indices = false, typename keys_it, typename vals_it, 
     typename segments_it>
   void blocksort_segments(keys_it keys, vals_it vals, segments_it segments, 
-    int num_segments, comp_t comp) {
+    int num_segments) {
 
     // Distribute the segment descriptors to different CTAs.
     mem_t<int> partitions = binary_search_partitions<bounds_lower>(segments, 
@@ -83,6 +83,7 @@ struct segsort_t {
     // Block sort the input. The position of the first and last segment 
     // descriptors are stored to merge_ranges.
 
+    comp_t comp = this->comp;
     int count = this->count;
     key_t* keys_blocksort = this->keys_blocksort;
     val_t* vals_blocksort = this->vals_blocksort;
@@ -411,7 +412,7 @@ void segmented_sort(key_t* keys, val_t* vals, int count, seg_it segments,
   detail::segsort_t<launch_arg_t, key_t, val_t, comp_t> 
     segsort(keys, vals, count, comp, context);
 
-  segsort.blocksort_segments(keys, vals, segments, num_segments, comp);
+  segsort.blocksort_segments(keys, vals, segments, num_segments);
   segsort.merge_passes();
 }
 
@@ -425,7 +426,7 @@ void segmented_sort_indices(key_t* keys, int* indices, int count,
     segsort(keys, indices, count, comp, context);
 
   segsort.template blocksort_segments<true>(keys, counting_iterator_t<int>(), 
-    segments, num_segments, comp);
+    segments, num_segments);
   segsort.merge_passes();
 }
 
